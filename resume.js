473d5d1af04696b1b5507d76303882da61c7c0c8
nmnkshyp4 <96513711+nmnkshyp4@users.noreply.github.com>
2022-11-17 22:51:43 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,112 @@
+let menuicon = document.getElementsByClassName("menuicon")[0];
+let menu = document.getElementsByClassName("menu")[0];
+let x = document.getElementsByClassName("x")[0];
+
+menuicon.addEventListener("click", () => {
+    menu.style.visibility = "visible";
+    menuicon.style.visibility = "hidden";
+    x.style.visibility = "visible";
+})
+
+x.addEventListener("click", () => {
+    menu.style.visibility = "hidden";
+    menuicon.style.visibility = "visible";
+    x.style.visibility = "hidden";
+})
+
+let work1 = document.getElementsByClassName("work1")[0];
+
+work1.addEventListener("mouseover", () => {
+    work1.style.filter = "blur(3.5px)";
+    work1.style.transform = "scale(1.02)";
+    work1.style.transitionDelay = "0.2s";
+})
+work1.addEventListener("mouseout", () => {
+    work1.style.filter = "blur(0px)";
+    work1.style.transform = "scale(1)";
+})
+
+let work2 = document.getElementsByClassName("work2")[0];
+
+work2.addEventListener("mouseover", () => {
+    work2.style.filter = "blur(3.5px)";
+    work2.style.transform = "scale(1.02)";
+    work2.style.transitionDelay = "0.2s"
+})
+work2.addEventListener("mouseout", () => {
+    work2.style.filter = "blur(0px)";
+    work2.style.transform = "scale(1)";
+})
+
+let work3 = document.getElementsByClassName("work3")[0];
+
+work3.addEventListener("mouseover", () => {
+    work3.style.filter = "blur(3.5px)";
+    work3.style.transform = "scale(1.02)";
+    work3.style.transitionDelay = "0.2s";
+})
+work3.addEventListener("mouseout", () => {
+    work3.style.filter = "blur(0px)";
+    work3.style.transform = "scale(1)";
+})
+
+let work4 = document.getElementsByClassName("work4")[0];
+
+work4.addEventListener("mouseover", () => {
+    work4.style.filter = "blur(3.5px)";
+    work4.style.transform = "scale(1.02)";
+    work4.style.transitionDelay = "0.2s";
+})
+work4.addEventListener("mouseout", () => {
+    work4.style.filter = "blur(0px)";
+    work4.style.transform = "scale(1)";
+})
+
+let work5 = document.getElementsByClassName("work5")[0];
+
+work5.addEventListener("mouseover", () => {
+    work5.style.filter = "blur(3.5px)";
+    work5.style.transform = "scale(1.02)";
+    work5.style.transitionDelay = "0.2s";
+})
+work5.addEventListener("mouseout", () => {
+    work5.style.filter = "blur(0px)";
+    work5.style.transform = "scale(1)";
+})
+
+let work6 = document.getElementsByClassName("work6")[0];
+
+work6.addEventListener("mouseover", () => {
+    work6.style.filter = "blur(3.5px)";
+    work6.style.transform = "scale(1.02)";
+    work6.style.transitionDelay = "0.2s";
+})
+work6.addEventListener("mouseout", () => {
+    work6.style.filter = "blur(0px)";
+    work6.style.transform = "scale(1)";
+})
+
+let work7 = document.getElementsByClassName("work7")[0];
+
+work7.addEventListener("mouseover", () => {
+    work7.style.filter = "blur(3.5px)";
+    work7.style.transform = "scale(1.02)";
+    work7.style.transitionDelay = "0.2s";
+})
+work7.addEventListener("mouseout", () => {
+    work7.style.filter = "blur(0px)";
+    work7.style.transform = "scale(1)";
+})
+
+let work8 = document.getElementsByClassName("work8")[0];
+
+work8.addEventListener("mouseover", () => {
+    work8.style.filter = "blur(3.5px)";
+    work8.style.transform = "scale(1.02)";
+    work8.style.transitionDelay = "0.2s";
+})
+work8.addEventListener("mouseout", () => {
+    work8.style.filter = "blur(0px)";
+    work8.style.transform = "scale(1)";
+})
+
